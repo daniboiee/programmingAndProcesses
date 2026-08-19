ca5f3bf6ca59cb8fe738c4b9302c954d79479197
Daniel Sarruf <daniel.sarruf@my.bdsc.school.nz>
2026-08-19 14:29:49 +1200

    general functions and main() made

--- BorrowingAssiter.py
+++ BorrowingAssiter.py
@@ -40,17 +40,68 @@ class Item:
         self.is_available = True
         print(f"Item has been succesfully returned.")
 
-item1 = Item("EQ001", "Casio Calculator", "Calculator")
+# Containers for items
+items = []
 
-print(item1.get_details())
+# General utility functions
+def clear_screen():
+    print("\n" * 40)
 
-item1.borrow("Matthew", datetime.datetime(2026, 8, 25))
+def pause():
+    input("\nPress Enter to continue...")
 
-print(item1.borrower)
-print(item1.borrow_date)
-print(item1.due_date)
-print(item1.is_available)
+def generate_id():
+    return f"EQ{len(items) + 1:03d}"
 
-item1.return_item()
+def find_item():
+    if len(items) == 0:
+        print("There are currently no items.")
+        return None
 
-print(item1.is_available)
+    print("\nAvailable items:")
+    for item in items:
+        print(item.get_details())
+
+    item_id = input("\nEnter the ID of the item: ").strip()
+
+    for item in items:
+        if item.id == item_id:
+            return item
+
+    print("No item with that ID was found.")
+    return None
+
+# Windows
+def add_item():
+    print()
+
+def edit_item():
+    print()
+    
+def delete_item():
+    print()
+
+def main():
+    while choice != 4:
+        clear_screen()
+
+        print("CLASSROOM EQUIPMENT TRACKER\n")
+        print("1. Add item")
+        print("2. Edit item")
+        print("3. Delete item")
+        print("4. Exit\n")
+
+        choice = input("Select an option: ").strip()
+
+        if choice == "1":
+            add_item()
+
+        elif choice == "2":
+            edit_item()
+
+        elif choice == "3":
+            delete_item()
+
+        elif choice != "4":
+            print("Invalid option.")
+            pause()
